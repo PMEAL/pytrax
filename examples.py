a98bf53f2e2888b536eab22fcd69a772ee098d84
Tom Tranter <t.g.tranter@gmail.com>
2018-11-19 21:45:33 -0500

    update examples to add cylinders

--- examples.py
+++ examples.py
@@ -13,14 +13,14 @@ import urllib.request as ur
 from io import BytesIO
 from PIL import Image
 
-save_figures = False
+save_figures = True
 global_stride = None
 plt.close('all')
 if __name__ == '__main__':
-    for image_run in [1]:
+    for image_run in [5]:
         if image_run == 0:
             # Open space
-            im = np.ones([3, 3], dtype=int)
+            im = np.ones([3, 3], dtype=np.uint8)
             fname = 'open_'
             num_t = 10000
             num_w = 10000
@@ -30,7 +30,7 @@ if __name__ == '__main__':
             url = 'https://i.imgur.com/nrEJRDf.png'
             file = BytesIO(ur.urlopen(url).read())
             im = np.asarray(Image.open(file))[:, :, 3] == 0
-            im = im.astype(int)
+            im = im.astype(np.uint8)
             im = np.pad(im, pad_width=50, mode='constant', constant_values=1)
             fname = 'tau_'
             # Number of time steps and walkers
@@ -49,22 +49,44 @@ if __name__ == '__main__':
                     image = tileandblank(image, n)
                 return image
 
-            im = np.ones([1, 1], dtype=int)
+            im = np.ones([1, 1], dtype=np.uint8)
             im = tileandblank(im, 4)
             fname = 'sierpinski_'
             # Number of time steps and walkers
             num_t = 2500
             num_w = 100000
             stride = 5
-        else:
+        elif image_run == 3:
             # Make an anisotropic image of 3D blobs
             im = ps.generators.blobs(shape=[300, 300, 300], porosity=0.5,
-                                     blobiness=[1, 2, 5]).astype(int)
+                                     blobiness=[1, 2, 5]).astype(np.uint8)
             fname = 'blobs_'
             # Number of time steps and walkers
             num_t = 10000
             num_w = 10000
             stride = 100
+        elif image_run == 4:
+            np.random.seed(1)
+            im = ps.generators.cylinders([300, 300, 300],
+                                         radius=10,
+                                         nfibers=100,
+                                         phi_max=90,
+                                         theta_max=90).astype(np.uint8)
+            fname = 'random_cylinders_'
+            num_t = 10000
+            num_w = 10000
+            stride = 10
+        elif image_run == 5:
+            np.random.seed(1)
+            im = ps.generators.cylinders([300, 300, 300],
+                                         radius=10,
+                                         nfibers=100,
+                                         phi_max=90,
+                                         theta_max=0).astype(np.uint8)
+            fname = 'aligned_cylinders_'
+            num_t = 10000
+            num_w = 10000
+            stride = 10
         print('Running Example: '+fname.strip('_'))
         # Override all strides
         if global_stride is not None:
@@ -72,7 +94,7 @@ if __name__ == '__main__':
         # Track time of simulation
         st = time.time()
         rw = pt.RandomWalk(im, seed=False)
-        rw.run(num_t, num_w, same_start=False, stride=stride)
+        rw.run(num_t, num_w, same_start=False, stride=stride, num_proc=6)
         print('run time', time.time()-st)
         rw.calc_msd()
         # Plot mean square displacement
@@ -95,10 +117,10 @@ if __name__ == '__main__':
                 # export to paraview
                 # pass
                 rw.export_walk(image=rw.im, sample=1)
-        nstrides = np.shape(rw.real_coords)[0]-1
-        steps = [np.int(np.floor(nstrides*i/4)) for i in np.arange(0, 5, 1)]
-        for step in steps:
-            rw.axial_density_plot(time=step, bins=50)
-            plt.title('Timestep: '+str(step*stride))
-            if save_figures:
-                rw._save_fig(fname+'density_'+str(step*stride)+'.png', dpi=dpi)
+#        nstrides = np.shape(rw.real_coords)[0]-1
+#        steps = [np.int(np.floor(nstrides*i/4)) for i in np.arange(0, 5, 1)]
+#        for step in steps:
+#            rw.axial_density_plot(time=step, bins=50)
+#            plt.title('Timestep: '+str(step*stride))
+#            if save_figures:
+#                rw._save_fig(fname+'density_'+str(step*stride)+'.png', dpi=dpi)
