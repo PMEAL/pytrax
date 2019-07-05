3921334f206410a17e9c0df54e66d5962cd2af98
tomtranter <t.g.tranter@gmail.com>
2019-07-05 16:08:24 +0100

    Update the cyclinders call again to make sure they are actually aligned

--- examples.py
+++ examples.py
@@ -24,10 +24,10 @@ global_stride = 1
 plt.close('all')
 if __name__ == '__main__':
     # Change number to run different example of include many in list
-    for image_run in range(5):
+    for image_run in [0, 1, 2]:
         if image_run == 0:
             # Open space
-            im = np.ones([3, 3], dtype=np.uint8)
+            im = np.ones([3, 3], dtype=np.int)
             fname = 'open_'
             num_t = 10000
             num_w = 10000
@@ -37,7 +37,7 @@ if __name__ == '__main__':
             url = 'https://i.imgur.com/nrEJRDf.png'
             file = BytesIO(ur.urlopen(url).read())
             im = np.asarray(Image.open(file))[:, :, 3] == 0
-            im = im.astype(np.uint8)
+            im = im.astype(np.int)
             im = np.pad(im, pad_width=50, mode='constant', constant_values=1)
             fname = 'tau_'
             # Number of time steps and walkers
@@ -56,7 +56,7 @@ if __name__ == '__main__':
                     image = tileandblank(image, n)
                 return image
 
-            im = np.ones([1, 1], dtype=np.uint8)
+            im = np.ones([1, 1], dtype=np.int)
             im = tileandblank(im, 4)
             fname = 'sierpinski_'
             # Number of time steps and walkers
@@ -66,30 +66,28 @@ if __name__ == '__main__':
         elif image_run == 3:
             # Make an anisotropic image of 3D blobs
             im = ps.generators.blobs(shape=[300, 300, 300], porosity=0.5,
-                                     blobiness=[1, 2, 5]).astype(np.uint8)
+                                     blobiness=[1, 2, 5]).astype(np.int)
             fname = 'blobs_'
             # Number of time steps and walkers
             num_t = 10000
             num_w = 10000
             stride = 100
         elif image_run == 4:
-            np.random.seed(1)
             im = ps.generators.cylinders([300, 300, 300],
                                          radius=10,
                                          ncylinders=100,
                                          phi_max=90,
-                                         theta_max=90).astype(np.uint8)
+                                         theta_max=90).astype(np.int)
             fname = 'random_cylinders_'
             num_t = 10000
             num_w = 10000
             stride = 10
         elif image_run == 5:
-            np.random.seed(1)
             im = ps.generators.cylinders([300, 300, 300],
                                          radius=10,
                                          ncylinders=100,
-                                         phi_max=90,
-                                         theta_max=0).astype(np.uint8)
+                                         phi_max=0,
+                                         theta_max=0).astype(np.int)
             fname = 'aligned_cylinders_'
             num_t = 10000
             num_w = 10000
@@ -101,7 +99,7 @@ if __name__ == '__main__':
         # Track time of simulation
         st = time.time()
         rw = pt.RandomWalk(im, seed=False)
-        rw.run(num_t, num_w, same_start=False, stride=stride, num_proc=6)
+        rw.run(num_t, num_w, same_start=False, stride=stride, num_proc=8)
         print('run time', time.time()-st)
         rw.calc_msd()
         # Plot mean square displacement
