6b8cae698841e70b2c319eb87192d2d11206ecfa
jgostick <jgostick@uwaterloo.ca>
2020-09-23 11:00:58 -0400

    working!

--- pytrax/funcs.py
+++ pytrax/funcs.py
@@ -1,68 +1,53 @@
+# %% Import packages and define helper functions
 import numpy as np
 import porespy as ps
 import matplotlib.pyplot as plt
 
 
-im = ps.generators.overlapping_spheres(shape=[250, 350], radius=5, porosity=0.9)
+def new_vector():
+    # Generate theta and phi
+    q, f = np.random.rand(2)*360
+    # Convert to axial components of a unit vector displacement
+    z = np.cos(np.deg2rad(f))
+    y = np.sin(np.deg2rad(f))*np.cos(np.deg2rad(q))
+    x = np.sin(np.deg2rad(f))*np.sin(np.deg2rad(q))
+    return x, y, z
+
+
+def get_start_point(im):
+    while True:
+        start = [np.random.randint(0, im.shape[i], 1) for i in range(im.ndim)]
+        start = np.array(start).flatten()
+        if im[tuple(start)]:
+            return start
+
+
+# %% Generate image
+
+im = ps.generators.overlapping_spheres(shape=[500, 500], radius=5, porosity=0.9)
 bd = ps.tools.get_border(im.shape, thickness=3, mode='faces')
 im[bd] = False
 
-# Generate starting points
-while True:
-    start = np.random.randint(0, 50, 2)
-    if im[tuple(start)]:
-        break
+# %% Run walk
 
-
-def new_end_point(start):
-    # Generate an end point
-    x_end = np.random.randint(0, im.shape[0], 1)
-    y_end = np.random.randint(0, im.shape[1], 1)
-    try:
-        z_end = np.random.randint(0, im.shape[2], 1)
-        end = np.array([x_end, y_end, z_end]).flatten()
-    except IndexError:
-        end = np.array([x_end, y_end]).flatten()
-
-    H = np.sqrt(np.sum((end-start)**2))
-    try:
-        f = np.rad2deg(np.arcsin((end-start)[2]/H))
-        h = H*np.cos(np.deg2rad(f))
-        q = np.rad2deg(np.arcsin((end-start)[1]/h))
-    except IndexError:
-        q = np.rad2deg(np.arcsin((end-start)[0]/H))
-        print(q)
-    try:
-        z = np.cos(np.deg2rad(f))
-        y = np.sin(np.deg2rad(f))*np.cos(np.deg2rad(q))
-        x = np.sin(np.deg2rad(f))*np.sin(np.deg2rad(q))
-        return x, y, z
-    except NameError:
-        y = np.cos(np.deg2rad(q))
-        x = np.sin(np.deg2rad(q))
-        return x, y
-
-
+mfp = 100  # mean free path
 im_path = np.zeros_like(im, dtype=int)
-x, y = new_end_point(start)
+start = get_start_point(im)
+x, y, z = new_vector()
 path = [start]
 loc = np.copy(start)
 i = 0
-while i < 10000:
+while i < 100000:
     i += 1
     new_loc = loc + np.array([x, y])
-    if im[tuple(new_loc.astype(int))] == False:
-        x, y = new_end_point(loc)
-        start = np.copy(loc)
-    elif np.sqrt(np.sum((new_loc-start)**2)) > 150:
-        x, y = new_end_point(loc)
+    check_1 = im[tuple(new_loc.astype(int))] == False
+    check_2 = np.sqrt(np.sum((new_loc-start)**2)) > mfp
+    if check_1 or check_2:
+        x, y, z = new_vector()
         start = np.copy(loc)
     else:
         loc = new_loc
         path.append(loc)
-        im_path[tuple(loc.astype(int))] = 1.0
+        im_path[tuple(loc.astype(int))] = i
 
-# for xy in path:
-    # im[tuple(xy.astype(int))] = False
-
-plt.imshow(im_path + (~im)*0.5, origin='xy')
+plt.imshow(im_path/im, origin='xy')
