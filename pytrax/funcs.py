0ccb0e6bd45779d3c0c34d3b3aee4eaccdc1d8b3
Jeff <jgostick@gmail.com>
2020-10-15 22:34:13 -0400

    adding tqdm to code, for progress bar

--- pytrax/funcs.py
+++ pytrax/funcs.py
@@ -3,6 +3,7 @@ import numpy as np
 import porespy as ps
 import matplotlib.pyplot as plt
 import imageio
+from tqdm import tqdm
 
 
 def new_vector(im, N=1):
@@ -51,51 +52,61 @@ def calculate_msd(path):
 im = ps.generators.overlapping_spheres(shape=[200, 400], radius=5, porosity=0.99)
 
 # %% Specify settings for walkers
-n_walkers = 100
-n_steps = 2000
-mean_free_path = 100
+n_walkers = 1000
+n_steps = 5000
+mean_free_path = 10
 
 # %% Run walk
 # Initialize arrays to store results, one image and one complete table
 im_path = np.zeros_like(im, dtype=int)
-path = np.zeros([n_steps, n_walkers, im.ndim], dtype=int)
+path = np.zeros([n_steps, n_walkers, im.ndim])
 # Generate the starting conditions
 start = get_start_point(im, n_walkers)
 x, y, z = new_vector(im, n_walkers)
 loc = np.copy(start)
 i = 0
-while i < n_steps:
-    # Determine trial location of each walker
-    if im.ndim == 3:
-        new_loc = loc + np.array([x,y,z])
-    else:
-        new_loc = loc + np.array([x, y])
-    # Check trial step for each walker
-    # check_1 = im[tuple(np.around(new_loc).astype(int))] == False
-    temp = wrap_indices(new_loc, im.shape)
-    # if np.any(new_loc.flatten() < 0):
-        # print(np.array(new_loc).flatten(), temp)
-    check_1 = im[temp] == False
-    check_2 = np.sqrt(np.sum((new_loc-start)**2, axis=0)) >= mean_free_path
-    # If either check found an invalid move, address it
-    if np.any(check_1) or np.any(check_2):
-        # Find the walker indices which have invalid moves
-        inds = np.where((check_1 == True) + (check_2 == True))
-        # Regenerate direction vectors for invalid walkers
-        x[inds], y[inds], z[inds] = new_vector(im, len(inds[0]))
-        # Update starting position for invalid walkers
-        start[:, inds] = np.around(loc[:, inds]).astype(int)
-        # The while loop will re-run so this step is ignored
-    else:  # If all walkers had a valid step, then execute the walk
-        # Update the location of each walker with trial step
-        loc = new_loc
-        # Record new position of walker in path array
-        path[i][:] = np.around(loc).astype(int).T
-        # Write walker position into image
-        # Only works for few walkers and limited step number
-        im_path[wrap_indices(loc, im.shape)] += 1
-        # Increment the step index
-        i += 1
+with tqdm(range(n_steps)) as pbar:
+    while i < n_steps:
+        # Determine trial location of each walker
+        if im.ndim == 3:
+            new_loc = loc + np.array([x,y,z])
+        else:
+            new_loc = loc + np.array([x, y])
+        # Check trial step for each walker
+        temp = wrap_indices(new_loc, im.shape)
+        check_1 = im[temp] == False
+        check_2 = np.sqrt(np.sum((new_loc-start)**2, axis=0)) > mean_free_path
+        # If either check found an invalid move, address it
+        if np.any(check_1) or np.any(check_2):
+            # Find the walker indices which have invalid moves
+            inds = np.where((check_1 == True) + (check_2 == True))
+            # Regenerate direction vectors for invalid walkers
+            x[inds], y[inds], z[inds] = new_vector(im, len(inds[0]))
+            # Update starting position for invalid walkers to current position
+            start[:, inds] = np.around(loc[:, inds]).astype(int)
+            # The while loop will re-run so this step is ignored
+        else:  # If all walkers had a valid step, then execute the walk
+            # Update the location of each walker with trial step
+            loc = new_loc
+            # Record new position of walker in path array
+            path[i][:] = loc.T
+            # Write walker position into image
+            # Only works for few walkers and limited step number
+            im_path[wrap_indices(loc, im.shape)] += 1
+            # Increment the step index
+            i += 1
+            pbar.update()
+
+# %%
+# The following is the distance each walker travels in each step...should
+# be 1.0 everywhere. Will be good for a unit test.
+# d = (path[1:, :, 0] - path[:-1, :, 0])**2 + (path[1:, :, 1] - path[:-1, :, 1])**2
+d = (path[1:, :, 0] - path[0, :, 0])**2 + (path[1:, :, 1] - path[0, :, 1])**2
+s = np.arange(1, n_steps, 10)
+fig = plt.plot(s, d[::10, :].mean(axis=1), '.')
+fig = plt.plot(s, s)
+
+
 # %%
 # Show the image of walkers
 plt.figure()
