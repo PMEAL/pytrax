c9949446314f3aa7be74d7aad650ec417a68d6b9
jgostick <jgostick@gmail.com>
2020-12-06 22:14:04 -0500

    fixing bugs

--- pytrax/funcs.py
+++ pytrax/funcs.py
@@ -18,10 +18,10 @@ def new_vector(im, N=1, L=1):
     return np.array((x, y, z))*L
 
 
-def get_start_point(im, N=1):
+def get_start_points(im, N=1):
     # Find all voxels in im that are not solid
     options = np.where(im)
-    # From this list of options, choice one for each walker
+    # From this list of options, choose one for each walker
     inds = np.random.randint(0, len(options[0]), N)
     # Convert the chosen list location into an x,y,z index
     points = np.array([options[i][inds] for i in range(im.ndim)])
@@ -49,22 +49,24 @@ def calculate_msd(path):
 
 
 # %% Generate image
-im = ps.generators.overlapping_spheres(shape=[400, 400], radius=10, porosity=0.65)
+np.random.seed(0)
+im = ps.generators.overlapping_spheres(shape=[500, 500], radius=8, porosity=0.65)
 im = ps.filters.fill_blind_pores(im)
 bd = ps.tools.get_border(shape=im.shape, mode='faces')
 im = ps.filters.trim_nonpercolating_paths(im, inlets=bd, outlets=bd)
-# im = np.ones_like(im)
+im = np.ones_like(im)
 
 # %% Specify settings for walkers
-n_walkers = 1000
-n_steps = 5000
+n_walkers = 5000
+n_steps = 1000
 mu = 1.73e-5  # Pa.s
 p = 101325  # Pa
 T = 298  # K
 R = 8.314  # J/mol.K
 MW = 0.0291  # kg/mol
 L = mu/p * np.sqrt(np.pi*R*T/(2*MW)) * 1e9  # nm
-res = 600   # nm/voxel
+# L = 1000
+res = 10000   # nm/voxel
 mfp = L/res  # voxel
 N_Av = 6.022e23  # Avagadro's constant
 k_B = 1.3806e-23  # Boltzmann's constant
@@ -72,10 +74,10 @@ v_rms = np.sqrt(3 * k_B * T / (MW / N_Av)) * 1e9  # nm/s
 
 # %% Run walk
 # Initialize arrays to store results, one image and one complete table
-im_path = np.zeros_like(im, dtype=int)
-path = np.zeros([n_steps, n_walkers, im.ndim])
+im_path = np.zeros_like(im, dtype=int)  # Image for drawing walker paths
+path = np.zeros([n_steps, n_walkers, im.ndim])  # Table for tracking positions
 # Generate the starting conditions
-start = get_start_point(im, n_walkers)
+start = get_start_points(im, n_walkers)
 x, y, z = new_vector(im, N=n_walkers, L=min(1, mfp))
 loc = np.copy(start)
 i = 0
@@ -87,7 +89,7 @@ with tqdm(range(n_steps)) as pbar:
         temp = wrap_indices(new_loc, im.shape)
         check_1 = im[temp] == False
         check_2 = np.sqrt(np.sum((new_loc-start)**2, axis=0)) > mfp
-        # If either check found an invalid move, address it
+        # If either check found invalid moves, address them
         if np.any(check_1) or np.any(check_2):
             # Find the walker indices which have invalid moves
             inds = np.where((check_1 == True) + (check_2 == True))
@@ -112,25 +114,24 @@ with tqdm(range(n_steps)) as pbar:
 # The following is the distance each walker travels in each step...should
 # be 1.0 everywhere. Will be good for a unit test.
 # d = (path[1:, :, 0] - path[:-1, :, 0])**2 + (path[1:, :, 1] - path[:-1, :, 1])**2
-d = np.sqrt((path[1:, :, 0] - path[0, :, 0])**2 +
-            (path[1:, :, 1] - path[0, :, 1])**2)
+d = np.sqrt(np.sum((path[1:, :, :] - path[0, :, :])**2, axis=2))
 d = d * res * 1e-9  # m
 f = min(1, mfp)
-s = np.sqrt(np.linspace(0, n_steps*f*res/v_rms, n_steps-1))
-fig = plt.plot(s[::10], d[::10, :].mean(axis=1), '.')
-plt.xlabel('sqrt(t) [s]')
-plt.ylabel('Average Displacement [m]')
+s = (np.linspace(0, n_steps*f*res/v_rms, n_steps-1))
+fig = plt.plot(s[::10], np.mean((d[::10, :])**2, axis=1), '.')
+plt.xlabel('Time [s]')
+plt.ylabel('Mean Squared Displacement [m]')
 
-D = (d[-1, :]**2).mean() / (6*n_steps*f*res/v_rms)
+D = ((d[-1, :])**2).mean() / (6*n_steps*f*res/v_rms)
 print(D)
 
 
 # %%
 # Show the image of walkers
-if 1:
+if 0:
     plt.figure()
     temp = np.log10(im_path+1)/im
     temp = np.tile(temp, [3, 3])
-    plt.imshow(temp, origin='xy', cmap=plt.cm.viridis, interpolation='none')
+    plt.imshow(temp, origin='xy', cmap=plt.cm.twilight_r, interpolation='none')
     plt.axis('tight')
 # imageio.volsave('Random_walk_3d.tif', (np.log10(im_path+0.1)/im).astype(int))
